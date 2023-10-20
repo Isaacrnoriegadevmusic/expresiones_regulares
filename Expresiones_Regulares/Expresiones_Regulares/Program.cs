859518e0cd3a36861bc174d2986ab349dd80c76f
Isaacrnoriegadevmusic <143848815+Isaacrnoriegadevmusic@users.noreply.github.com>
2023-10-19 20:21:51 -0600

    Add files via upload

---
+++ Expresiones_Regulares/Expresiones_Regulares/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string patronCorreo = @"^([\w\.-]+)@(gmail\.com|hotmail\.com)$";
+
+        while (true)
+        {
+            Console.WriteLine("Escribe una dirección de correo (Gmail o Hotmail): ");
+            string correo = Console.ReadLine();
+
+            if (Regex.IsMatch(correo, patronCorreo))
+            {
+                Console.WriteLine("Correo válido.");
+            }
+            else
+            {
+                Console.WriteLine("Correo inválido. ¿Deseas ingresar otro correo? (S/N)");
+                string respuesta = Console.ReadLine();
+
+                if (respuesta.Trim().Equals("N", StringComparison.OrdinalIgnoreCase))
+                {
+                    break; // Salir del bucle si el usuario no desea ingresar otro correo
+                }
+            }
+        }
+
+        string patronCURP = @"^([A-Z]{4})([0-9]{6})([H|M])([A-Z]{2})([A-Z]{4})([0-9]{1})";
+
+        while (true)
+        {
+            Console.WriteLine("Escribe una CURP: ");
+            string CURP = Console.ReadLine();
+
+            if (Regex.IsMatch(CURP, patronCURP))
+            {
+                Console.WriteLine("CURP válida.");
+            }
+            else
+            {
+                Console.WriteLine("CURP inválida. ¿Deseas ingresar otra CURP? (S/N)");
+                string respuesta = Console.ReadLine();
+
+                if (respuesta.Trim().Equals("N", StringComparison.OrdinalIgnoreCase))
+                {
+                    break; // Salir del bucle si el usuario no desea ingresar otra CURP
+                }
+            }
+        }
+    }
+}
---
+++ Expresiones_Regulares/Expresiones_Regulares/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Expresiones_Regulares")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Expresiones_Regulares")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("ec755e7d-566d-4975-a55e-2c70fdc64578")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
